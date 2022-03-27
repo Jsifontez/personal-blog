dc77e9f84884432e008cc67ecc51ce100db7c53b
jsifontez <juan.sifontez.dev@gmail.com>
2022-03-27 03:11:44 -0400

    update: Add new seo component
    Create a config file for next
    Delete the old seo component
    Create a new seo component with next specifications

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export default {
+  title: 'Juan Sifontez Blog',
+  author: {
+    name: 'Juan Sifontez',
+    summary: 'frontend developer specialized in React.js',
+  },
+  description: 'Tech and personal blog to share my knowledge.',
+  social: {
+    twitter: 'jsifontez_',
+  },
+}
