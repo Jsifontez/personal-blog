f0e70bfc6757a315124777a0549615a17926d762
jsifontez <juan.sifontez.dev@gmail.com>
2022-04-01 22:36:23 -0400

    fix: Add active link in header
    use useRouter hook in header component
    remove links style from header.module.css
    copy link styles in global.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 import styles from "./header.module.css"
 
 const Header = () => {
+  const router = useRouter()
   const links = [
     {to: '/', name: 'About'},
-    {to: '/projects/', name: 'Projects'},
-    {to: '/blog/', name: 'Blog'},
-    {to: '/contact/', name: 'Contact'}
+    {to: '/projects', name: 'Projects'},
+    {to: '/blog', name: 'Blog'},
+    {to: '/contact', name: 'Contact'}
   ]
 
   return(
@@ -19,12 +21,13 @@ const Header = () => {
       <nav className={styles.header__nav}>
         <ul className={styles.header__links}>
           {links.map(link => (
-            <li className={styles.header__link} key={link.name}>
-              <Link
-                activeClassName="active"
-                href={link.to}
-              >
-                <a className="link link--header">
+            <li
+              className={styles.header__link}
+              key={link.name}>
+              <Link href={link.to}>
+                <a
+                  className={`link link--header ${router.pathname == link.to ? "active" : ''}`}
+                >
                   {link.name}
                 </a>
               </Link>
